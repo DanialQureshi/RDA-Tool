6f86db562e51bbecbe5d2967d2a1a20fb8596899
Justin Cosgrove <jcosgrove19@gmail.com>
2016-03-24 14:55:34 -0400

    add readfile class

--- RdaTool/src/com/pp/Sandbox.java
+++ RdaTool/src/com/pp/Sandbox.java
@@ -5,10 +5,42 @@
  */
 package com.pp;
 
+import com.pp.jst.ReadFile;
+import java.util.ArrayList;
 /**
  *
  * @author jcosgrove
  */
 public class Sandbox {
+    
+    private static String fileName = "test.txt";
+    private static String temp;
+    private static ArrayList<String> al = new ArrayList();
+    
+    public static void main(String[] args){
+        
+        
+        ReadFile rf = new ReadFile(fileName);
+
+        
+       
+        temp = rf.GetNextLine();
+        while (temp != null)
+        {
+            al.add(temp);
+            temp = rf.GetNextLine();
+            
+        }
+        
+
+        for (String a : al){
+            System.out.println(a);
+            
+        }
+        
+        
+        
+        
+    }
     
 }
